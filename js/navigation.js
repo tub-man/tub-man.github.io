7b6520b71c7c8252151bfd14197a14bb874eeb2e
Bram <bram.gagliardi@gmail.com>
2016-12-28 15:12:19 -0500

    Added sticky nav

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,9 @@
 $(function() {
+    //  sticky nav
+    var sticky = new Waypoint.Sticky({
+        element: $('nav.desktop')[0]
+    })
+
     $('nav.mobile').on('click', function() {
         $(this).toggleClass('active');
         $(this).find('ul').stop().slideToggle();
